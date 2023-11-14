87fcc81c63926a8984eeae33be1277b8ace327dc
setap <khuseyn693@gmail.com>
2023-11-14 23:24:48 +0400

    last

--- task/src/components/Home.jsx
+++ task/src/components/Home.jsx
@@ -5,18 +5,20 @@ import { ROUTER } from "../constant/Router";
 const Home = () => {
   return (
     <div className="main ref">
-     <div className="sides">
-     <div className="side">
-        <Link to={ROUTER.Contact}>Contact and Products</Link>
-        <Link to={ROUTER.About}>About Us</Link>
-        <Link to={ROUTER.Blog}>Blog</Link>
+      <div className="sides">
+        <div className="side">
+          <Link to={ROUTER.Contact}>
+            <div className="flex column">Products</div>
+          </Link>
+          <Link to={ROUTER.About}>About Us</Link>
+          <Link to={ROUTER.Blog}>Blog</Link>
+        </div>
+        <div className="side">
+          <Link to={ROUTER.Soda}>Soda</Link>
+          <Link to={ROUTER.Chips}>Chips</Link>
+          <Link to={ROUTER.Sardines}>Fresh Sardines</Link>
+        </div>
       </div>
-      <div className="side">
-        <Link to={ROUTER.Soda}>Soda</Link>
-        <Link to={ROUTER.Chips}>Chips</Link>
-        <Link to={ROUTER.Sardines}>Fresh Sardines</Link>
-      </div>
-     </div>
     </div>
   );
 };
