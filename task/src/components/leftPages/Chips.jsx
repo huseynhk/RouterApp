ee4a60f4cd6a76ad852aba9cd9378f3b4c4b3c73
setap <khuseyn693@gmail.com>
2023-11-15 15:13:36 +0400

    deleteImgs

--- task/src/components/leftPages/Chips.jsx
+++ task/src/components/leftPages/Chips.jsx
@@ -10,19 +10,38 @@ const Chips = () => {
     setImages([...images, Lays]);
   };
 
+  const deleteImg = (index) => {
+    const deletedImage = [...images];
+    deletedImage.splice(index, 1);
+    setImages(deletedImage);
+    // setImages((prevImage) =>
+    //   prevImage.filter((id, imgIndex) => imgIndex !== index)
+    // );
+  };
+
   return (
     <>
       <div className="main flex">
         <div className="image-container">
           {images.map((img, index) => (
-            <img key={index} src={img} className="lays" alt="Lays" />
+            <div key={index}>
+              <img src={img} className="lays" alt="Lays" />
+              <button className="nom" onClick={() => deleteImg(index)}>
+                Delete
+              </button>
+            </div>
           ))}
         </div>
         <div className="side fixed">
           <h2>Bags Eaten: {images.length}</h2>
-          <button className="nom" onClick={addImg}>
-            Nom Nom Nom
-          </button>
+          <div className="flex">
+            <button className="nom" onClick={addImg}>
+              Nom Nom
+            </button>
+            <button className="nom" onClick={() => setImages([])}>
+              Enough
+            </button>
+          </div>
           <Link to={ROUTER.Home}>Go Back</Link>
         </div>
       </div>
